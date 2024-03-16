e747b1a9f7cf7e903b508356a42babd625570537
saidulixlam <saidulixlam@gmail.com>
2024-03-17 01:40:34 +0530

    With SwipeableViews

--- src/components/Restaurent.js
+++ src/components/Restaurent.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SwipeableViews from 'react-swipeable-views';
 import { Link } from 'react-router-dom';
-import { IoWalletOutline } from "react-icons/io5";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDrupal } from '@fortawesome/free-brands-svg-icons';
 import { BiSolidOffer } from "react-icons/bi";
-import './Swipe.css';
+import { IoWalletOutline } from "react-icons/io5";
 
 const Restaurant = ({ name, address, image }) => {
     return (
@@ -19,12 +21,12 @@ const Restaurant = ({ name, address, image }) => {
             <img src={image} alt="Restaurant pic" style={{
                 objectFit: "cover",
                 width: "100%",
-                height: '120px',
+                height: '150px',
                 borderTopLeftRadius: '14px',
                 borderTopRightRadius: '14px'
             }} />
 
-            <div className="card-body bg-white bg-opacity-75 p-2 my-3" style={{ maxHeight: '100px', overflow: 'hidden' }}>
+            <div className="card-body bg-white bg-opacity-75 p-2 my-3" style={{ maxHeight: '30%', overflow: 'hidden' }}>
                 <h6 className="card-title">{name}</h6>
                 <span className="card-text">Address: {address}</span>
             </div>
@@ -35,7 +37,7 @@ const Restaurant = ({ name, address, image }) => {
 
 const Popular = ({ name, ratings, image }) => {
     return (
-        <div className=" p-2 card mb-4 border-0 shadow-md my-2 d-flex flex-row align-items-center" style={{ borderRadius: '14px', height: '40%', listStyle: 'none' }}>
+        <div className=" p-2 card mb-4 border-0 shadow-md my-2 d-flex flex-row align-items-center" style={{borderRadius: '14px', height: '40%', listStyle: 'none' }}>
             <img src={image} alt="Restaurant" style={{ objectFit: "cover", width: "50%", height: '165px', borderRadius: '14px' }} />
             <div className="bg-white bg-opacity-75 mx-5 mb-3" style={{ width: '50%', height: "140px" }}>
                 <h6 className="card-title">{name}</h6>
@@ -50,37 +52,11 @@ const Popular = ({ name, ratings, image }) => {
 
 
 const RestaurantList = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [startX, setStartX] = useState(0);
-
     const [restaurants, setRestaurants] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const token = localStorage.getItem('token');
 
-    const handleTouchStart = useCallback((event) => {
-        setStartX(event.touches[0].clientX);
-    }, []);
-
-    const handleTouchMove = useCallback((event) => {
-        const currentX = event.touches[0].clientX;
-        const difference = startX - currentX;
-
-        // Adjust sensitivity factor (e.g., 50)
-        const sensitivity = 50;
-
-        if (difference > sensitivity && currentIndex < restaurants.length - 1) {
-            setCurrentIndex(currentIndex + 1);
-            setStartX(currentX); // Update startX to currentX to maintain smooth movement
-        } else if (difference < -sensitivity && currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            setStartX(currentX); // Update startX to currentX to maintain smooth movement
-        }
-    }, [currentIndex, restaurants.length, startX]);
-
-    const handleTouchEnd = useCallback(() => {
-        setStartX(0);
-    }, []);
     useEffect(() => {
         const fetchRestaurants = async () => {
             try {
@@ -94,6 +70,7 @@ const RestaurantList = () => {
                 );
                 const { data } = response;
                 const restaurantsList = data.map(({ restaurant_id, restaurant_name, location, images, rating }) => ({
+
                     restaurant_id,
                     restaurant_name,
                     rating: rating.restaurant_avg_rating,
@@ -117,20 +94,28 @@ const RestaurantList = () => {
 
     return (
         <div className='p-2 mt-5' style={{ minHeight: '100vh' }}>
+
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-lg">
                 <div className="container mx-2">
+
                     <div className="d-flex flex-column">
+
                         <div className="d-flex align-items-center">
                             <span>Pre Order From</span>
+                            
                         </div>
+
                         <h3 className="mb-0">Order from New Delhi</h3>
                     </div>
+
                     <div className="ml-auto mr-1">
-                        <h2>X</h2>
+                        <FontAwesomeIcon icon={faDrupal} style={{ color: '#FFD43B', fontSize: '2em' }} />
                     </div>
                 </div>
             </nav>
 
+
+            {/* User information section */}
             <div className="container mt-5 d-flex justify-content-between align-items-center bg-light shadow p-2 rounded ml-2">
                 <div className='d-flex flex-column bg-light shadow-sm p-1 rounded'>
                     <h3 className='text-muted'>John</h3>
@@ -142,38 +127,34 @@ const RestaurantList = () => {
                 </div>
             </div>
 
+            {/* User Taste section */}
             <div className="p-1 mt-4 mx-1">
                 <div className='d-flex justify-content-between align-center'>
-                    <h3 className='p-2'>Your Taste</h3>
+                    <h3 className='p-2'>
+                        Your Taste
+                    </h3>
                     <button className='btn btn-transparent '>
                         <span>View All {'>'}</span>
                     </button>
                 </div>
 
-                {restaurants.length > 0 &&
-                    <div className="swipeable-container">
-                        <div
-                            className="swipeable-content"
-                            onTouchStart={handleTouchStart}
-                            onTouchMove={handleTouchMove}
-                            onTouchEnd={handleTouchEnd}
-                            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-                        >
-                            {restaurants.map((restaurant, index) => (
-                                <div key={index} className="col-5 gap-2 mb-2 swipeable-item" style={{maxHeight:'270px',overflow:'hidden'}}>
-                                    <Link to={`/detail/${restaurant.restaurant_id}`} style={{ textDecoration: 'none' }}>
-                                        <Restaurant
-                                            image={restaurant.image}
-                                            name={restaurant.restaurant_name}
-                                            address={restaurant.location?.location_address || null}
-                                            rating={restaurant.rating}
-                                        />
-                                    </Link>
-                                </div>
-                            ))}
-                        </div>
+                {/* swipable section */}
+                <SwipeableViews axis='x'>
+                    <div className="d-flex flex-nowrap" style={{ height: '260px' }}>
+                        {restaurants.map((restaurant, index) => (
+                            <div key={index} className="col-5 gap-2 mb-2" >
+                                <Link to={`/detail/${restaurant.restaurant_id}`} style={{ textDecoration: 'none' }}>
+                                    <Restaurant
+                                        image={restaurant.image}
+                                        name={restaurant.restaurant_name}
+                                        address={restaurant.location?.location_address || null}
+                                        rating={restaurant.restaurant_mode}
+                                    />
+                                </Link>
+                            </div>
+                        ))}
                     </div>
-                }
+                </SwipeableViews>
             </div>
             <div className="mx-1 mt-3 p-2">
                 <h3 className=' mx-2'>Popular ones</h3>
